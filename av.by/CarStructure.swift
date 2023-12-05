9fd7d93a32437ac8d2c69ed6ca518249d8de0c3a
malyshewandrew <malyshewandrew@protonmail.com>
2023-12-05 17:02:20 +0300

    add Car Structure + Enums

--- av.by/CarStructure.swift
+++ av.by/CarStructure.swift
@@ -1,1 +1,65 @@
 import Foundation
+import UIKit
+
+// MARK: - STRUCTURE FOR NEW CAR:
+
+struct Car {
+    var name: String
+    var price: Int
+    var dollar: Double {
+        return Double(price / 3)
+    }
+    var photos: [UIImage]
+    var year: Int
+    var typeTransmission: TypeTransmission
+    var sizeEngine: Double
+    var typeEngine: TypeEngine
+    var typeBody: TypeBody
+    var wheelDrive: TypeWheelDrive
+    var color: UIColor
+    var odometer: Int
+    var vin: Bool
+    var top: Bool
+    var city: String
+    var description: String
+    var complectation: String
+    var change: String
+    var lizing: Double {
+        return Double(Double(price / 3) * 1.3)
+    }
+    var seller: String
+}
+
+// MARK: - ENUMS FOR CAR SETTINGS:
+
+enum TypeTransmission: String {
+    case manual = "механика"
+    case automatic = "автомат"
+    case electric = "электро"
+    case hybrid = "гибрид"
+}
+
+enum TypeEngine: String {
+    case gasoline = "бензин"
+    case diesel = "дизель"
+    case electric = "электро"
+    case hybrid = "гибрид"
+    case gas = "газ"
+}
+
+enum TypeBody: String {
+    case sedan = "седан"
+    case coupe = "купе"
+    case hatchback = "хетчбек"
+    case convertible = "кабриолет"
+    case pickup = "пикап"
+    case wagon = "грузовик"
+    case crossover = "джип"
+    case minivan = "минивен"
+}
+
+enum TypeWheelDrive: String {
+    case frontWheelDrive = "передний привод"
+    case rearWheelDrive = "задний привод"
+    case allWheelDrive = "полный привод"
+}
