5937c600b194204bc5d3ab485e382688dc19e19f
malyshewandrew <malyshewandrew@protonmail.com>
2023-12-06 18:10:41 +0300

    add collection view for custom cell

--- av.by/CellSearch.swift
+++ av.by/CellSearch.swift
@@ -12,12 +12,19 @@ class CellSearch: UITableViewCell {
     private let priceBynSymbolLabel = UILabel()
     private let priceUsdLabel = UILabel()
 
+    private var collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+
+    private let descriptionView = UIView()
+    private let descriptionLabel = UILabel()
+
     // MARK: - HELPERS:
 
     func helpers() {
         // MARK: - VIEW:
 
-        contentView.addSubviews(view, nameLabel, bookmarkButton, priceBynLabel, priceBynSymbolLabel, priceUsdLabel)
+        contentView.addSubviews(view)
+        view.addSubviews(nameLabel, bookmarkButton, priceBynLabel, priceBynSymbolLabel, priceUsdLabel, collectionView, descriptionView)
+        descriptionView.addSubviews(descriptionLabel)
     }
 
     // MARK: - CONFIGURE CONSTRAINS:
@@ -52,21 +59,45 @@ class CellSearch: UITableViewCell {
         priceBynLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5).isActive = true
         priceBynLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
         priceBynLabel.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        
+
         // MARK: - PRICE BYN SYMBOL LABEL:
 
         priceBynSymbolLabel.translatesAutoresizingMaskIntoConstraints = false
-        priceBynSymbolLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        priceBynSymbolLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
         priceBynSymbolLabel.leadingAnchor.constraint(equalTo: priceBynLabel.trailingAnchor, constant: 5).isActive = true
         priceBynSymbolLabel.heightAnchor.constraint(equalToConstant: 35).isActive = true
 
         // MARK: - PRICE USD LABEL:
 
         priceUsdLabel.translatesAutoresizingMaskIntoConstraints = false
-        priceUsdLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5).isActive = true
-        priceUsdLabel.leadingAnchor.constraint(equalTo: priceBynSymbolLabel.trailingAnchor, constant: 10).isActive = true
+        priceUsdLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        priceUsdLabel.leadingAnchor.constraint(equalTo: priceBynSymbolLabel.trailingAnchor, constant: 20).isActive = true
         priceUsdLabel.heightAnchor.constraint(equalToConstant: 35).isActive = true
         priceUsdLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+
+        // MARK: - COLLECTION VIEW:
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: priceBynLabel.bottomAnchor, constant: 10).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: 240).isActive = true
+        collectionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.97).isActive = true
+
+        // MARK: - DESCRIPTION VIEW:
+
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionView.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 5).isActive = true
+        descriptionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        descriptionView.heightAnchor.constraint(greaterThanOrEqualTo: descriptionLabel.heightAnchor, constant: 10).isActive = true
+        descriptionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.95).isActive = true
+
+        // MARK: - DESCRIPTION LABEL:
+
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.topAnchor.constraint(equalTo: descriptionView.topAnchor, constant: 0).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+
     }
 
     // MARK: - CONFIGURE UI:
@@ -95,18 +126,31 @@ class CellSearch: UITableViewCell {
         priceBynLabel.adjustsFontSizeToFitWidth = true
         priceBynLabel.font = .systemFont(ofSize: 26, weight: .black, width: .standard)
         priceBynLabel.text = "17845"
-        
+
         // MARK: - PRICE BYN SYMBOL LABEL:
 
         priceBynSymbolLabel.adjustsFontSizeToFitWidth = true
         priceBynSymbolLabel.font = .systemFont(ofSize: 14, weight: .bold, width: .standard)
         priceBynSymbolLabel.text = "р."
-        
+
         // MARK: - PRICE USD LABEL:
 
         priceUsdLabel.adjustsFontSizeToFitWidth = true
         priceUsdLabel.textColor = .gray
+        priceUsdLabel.font = .systemFont(ofSize: 16, weight: .regular, width: .standard)
         priceUsdLabel.text = "5599"
+
+        // MARK: - COLLECTION VIEW:
+
+        collectionView.layer.cornerRadius = 5
+        collectionView.backgroundColor = .clear
+
+        // MARK: - DESCRIPTION VIEW:
+        
+        // MARK: - DESCRIPTION LABEL:
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.font = .systemFont(ofSize: 15, weight: .regular, width: .standard)
+        descriptionLabel.text = "2018, автомат, 3.0, дизель, внедорожник, 169 000 км"
     }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
---
+++ av.by/CollectionViewCell.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+final class CollectionViewCell: UICollectionViewCell {
+    // MARK: - PROPERTIES:
+
+    private let imageView = UIImageView()
+    private let viewButton = UIButton()
+
+    var onTap: (() -> ())?
+
+    // MARK: - HELPERS:
+
+    private func helpers() {
+        addSubview(imageView)
+        addSubview(viewButton)
+    }
+
+    // MARK: - CONFIGURE CONSTRAINS:
+
+    private func configureConstrains() {
+        // MARK: - IMAGE VIEW:
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        // MARK: - VIEW BUTTON:
+
+        viewButton.translatesAutoresizingMaskIntoConstraints = false
+        viewButton.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        viewButton.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        viewButton.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        viewButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        viewButton.backgroundColor = .clear
+        viewButton.addTarget(self, action: #selector(funcTest), for: .touchUpInside)
+    }
+
+    // MARK: - CONFIGURE UI:
+
+    private func configureUI() {
+        // MARK: - IMAGE VIEW:
+
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+    }
+
+    // MARK: - LIFECYCLE:
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        helpers()
+        configureConstrains()
+        configureUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - IMAGES:
+
+    func setImage(_ image: UIImage) {
+        imageView.image = image
+    }
+
+    @objc func funcTest() {
+        onTap?()
+    }
+}
